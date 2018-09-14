03ead3b00f339c1f3c85529f2f0deadd7193198d
zeroincombenze <info@shs-av.com>
2018-09-14 13:47:01 +0200

    [TEST] Testing

---
+++ theme_odoo/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': "theme_odoo",
+
+    'summary': """Odoo Original Skin""",
+
+    'description': """
+(en) Odoo Original Skin
+-----------------------
+
+
+(it) Skin originale di Odoo
+---------------------------
+    """,
+
+    'author': "Odoo S.A.",
+    'website': "https://www.odoo.com/",
+
+    'category': 'Hidden',
+    'version': '7.0.0.0.1',
+    'depends': ['web'],
+    'installable': False
+}
---
+++ theme_zeroincombenze/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Antonio M. Vigliotti <antoniomaria.vigliotti@gmail.com>
+#
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': "theme_zeroincombenze",
+
+    'summary': """Zeroincombenze(R) Skin for Odoo""",
+
+    'description': """
+(en) Theme/Skin for Odoo 7.0 example
+------------------------------------
+
+
+(it) Esempio di tema per Odoo 7.0
+---------------------------------
+    """,
+
+    'author': "SHS-AV s.r.l.",
+    'website': "https://www.zeroincombenze.it/",
+
+    'category': 'Hidden',
+    'version': '7.0.0.0.1',
+    'depends': ['web'],
+    'installable': False
+}
